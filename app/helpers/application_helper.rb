a80ab289eff7da0db6d74c8bda8f3e7b3ed1849c
Chris Candelora <ccandelora@ccandelora-mbp.bcpress.lan>
2012-03-28 11:28:23 -0400

    updating master branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+# Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+  base_logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  base_logo
+  end
 end
